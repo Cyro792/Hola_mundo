7185aeb129f67f67c7bf43fe4f453838a1ca4491
Cyro792 <ba.caros@duocuc.cl>
2025-06-26 10:50:47 -0400

    Add files via upload

--- ejercicio.py
+++ ejercicio.py
@@ -1,6 +1,8 @@
 def suma(a,b):
     return a+b
-
+def resta(a,b):
+    return a-b
 n1=int(input("ingresa un numero: "))
 n2=int(input("ingresa un numero: "))
 print(suma(n1,n2))
+print(resta(n1,n2))
